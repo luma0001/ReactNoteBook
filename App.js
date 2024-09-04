d5c37b1171f84c0d0dc1dcace19b39719c6d22d5
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-09-04 12:12:40 +0200

    1 til 4

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  StyleSheet,
+  FlatList,
+  Text,
+  View,
+  TextInput,
+  Button,
+} from "react-native";
 
 export default function App() {
+  const [text, setText] = useState("");
+  const [notes, setNotes] = useState([]);
+
+  function buttonPressed() {
+    setNotes([...notes, { key: notes.length, name: text }]);
+    setText("");
+  }
+
+  const renderItem = ({ item, index }) => (
+    <Text>
+      {index + 1}. {item.name}
+    </Text>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={(input) => setText(input)}
+        value={text}
+      />
+      <Button title="Submit" onPress={buttonPressed}></Button>
+      <FlatList
+        data={notes}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.key}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +45,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 200,
+  },
+  textInput: {
+    backgroundColor: "lightblue",
+    minWidth: 200,
   },
 });
